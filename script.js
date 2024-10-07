47b88cae3e3119abcb1d339557b2dd5ab82f3dad
MiguelLoaizaCHURRO <loaiza.miguel@correounivalle.edu.co>
2024-10-07 09:10:51 -0500

    Ejemplo primera clase

---
+++ script.js
@@ -0,0 +1,15 @@
+// Utiliza prompt() para pedir el nombre y la edad del usuario
+var nombre = prompt("¿Cual es tu nombre?");
+var edad = prompt("¿Cuantos años tienes?");
+
+// Declara una variable llamada ciudad y asígnale el valor "Quito"
+var ciudad = "Quito";
+
+// Usa console.log() para mostrar las variables en la consola del navegador
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Ciudad: " + ciudad);
+
+// Muestra un mensaje de bienvenida utilizando alert()
+document.write("Hola " + nombre + ", tienes " + edad + " años y vives en " + ciudad);
+alert("Hola " + nombre + ", tienes " + edad + " años y vives en " + ciudad);
